befeb2fa20e511ff267c56737d946a86b93e5fd4
sidharth1213 <siddharth.soni@cobold.in>
2023-04-14 16:03:36 +0530

    ss:  added type

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
-
- const Items : any = ({mytask}:any)=> {
-  return (
-    <div className='bg-purple-300 w-1/2 my-2 border border-red-700  flex justify-between text-start gap-5 p-5'>
-        <div> 
-            <p className=' font-bold text-red-700'>{mytask.task}</p>
-            <p className='text-xs'>{mytask.description} </p>
-        </div>
-        <button className='text-blue-700 hover:text-blue-500'>Edit</button>
-        <button className='text-red-800 hover:text-red-500'>Delete</button>
-    </div>
-  )
+import React from "react";
+interface props {
+  mytask: {
+    task: String;
+    description: string;
+  };
 }
-export default Items
+const Items: React.FC<props> = ({ mytask }: props) => {
+  return (
+    <div className="bg-purple-300 w-1/2 my-2 border border-red-700  flex justify-between text-start gap-5 p-5">
+      <div>
+        <p className=" font-bold text-red-700">{mytask.task}</p>
+        <p className="text-xs">{mytask.description} </p>
+      </div>
+      <button className="text-blue-700 hover:text-blue-500">Edit</button>
+      <button className="text-red-800 hover:text-red-500">Delete</button>
+    </div>
+  );
+};
+export default Items;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,33 +1,35 @@
-import React, { useEffect, useState }  from "react";
+import React, { useEffect, useState } from "react";
 import Items from "./Items";
-
+interface mytask {
+  mytask: object;
+}
 function Main() {
-const [inputValues,setValues]=useState({
-  task:"",
-  description:"",
-})
-const [listArr,setList]=useState<object[]>([]);
-
-const setTask= (e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) :void =>{
-  setValues({
-    ...inputValues,
-    [e.target.name]:e.target.value
+  const [inputValues, setValues] = useState({
+    task: "",
+    description: "",
   });
-  console.log(inputValues);
-}
-const setArr=() =>{
-  setList([...listArr,inputValues])
-  
-}
-  return (
+  const [listArr, setList] = useState<object[]>([]);
 
-    <div className= "container-full p-5 text-center ">
-      <div className= "flex flex-col w-1/2 mx-auto items-center text-white">
-        <h3 className= "text-2xl font-bold text-black">Todo App</h3>
+  const setTask = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ): void => {
+    setValues({
+      ...inputValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const setArr = () => {
+    setList([...listArr, inputValues]);
+    setValues({ task: "", description: "" });
+  };
+  return (
+    <div className="container-full p-5 text-center ">
+      <div className="flex flex-col w-1/2 mx-auto items-center text-white">
+        <h3 className="text-2xl font-bold text-black">Todo App</h3>
         <input
-          type= "text"
-          placeholder= "Enter Task title"
-          className= "p-3 w-full border border-blue-700 rounded mt-2 bg-blue-950  placeholder:text-gray-300"
+          type="text"
+          placeholder="Enter Task title"
+          className="p-3 w-full border border-blue-700 rounded mt-2 bg-blue-950  placeholder:text-gray-300"
           value={inputValues.task}
           onChange={setTask}
           name="task"
@@ -38,21 +40,19 @@ const setArr=() =>{
           value={inputValues.description}
           onChange={setTask}
           name="description"
-
         />
-      <button className="bg-green-900 active:bg-green-700 mt-5 p-3 w-fit rounded " onClick={setArr}>
+        <button
+          className="bg-green-900 active:bg-green-700 mt-5 p-3 w-fit rounded "
+          onClick={setArr}
+        >
           ADD TODO
         </button>
       </div>
       <div className="flex flex-col items-center mt-10">
-        {
-          listArr.map((myval)=>{
-            return <Items mytask={myval}/>
-          })
-          
-        }
+        {listArr.map((myval: any, index) => {
+          return <Items mytask={myval} key={index} />;
+        })}
       </div>
-     
     </div>
   );
 }
